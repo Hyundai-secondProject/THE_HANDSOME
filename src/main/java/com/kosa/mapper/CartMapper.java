fa5ba2586d211350b99af03f6bac86d8613bc2dd
김민찬 <81581362+nsmchan@users.noreply.github.com>
2022-10-19 10:12:45 +0900

    [fix] cart select sql (#45)
    cartMapper.xml의 sql문 수정
    행이 하나밖에 조회가 안되는것을 result map으로 수정하여 여러행 조회가능하게 변경
    mapper, service test까지 완료

--- src/main/java/com/kosa/mapper/CartMapper.java
+++ src/main/java/com/kosa/mapper/CartMapper.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import com.kosa.domain.member.CartVO;
 
@@ -35,20 +34,11 @@ import com.kosa.domain.member.CartVO;
 
 @Mapper
 public interface CartMapper {
-		
-//	@Select("select * from cart")     // xml 문에서 sql문 처리했으니
-	public List<CartVO> select(String mid); // select 테스트
-	public List<CartVO> select2(String mid); // select 테스트2
-	public void insert(CartVO cart); // insert 테스트
+	
+	public List<CartVO> selectList(String mid); // select 
+	public void insert(CartVO cart); // insert 
 	// 스프링 mybatis에서는 param이 1개밖에 들어가지 않아서 여러개를 할때는 @Param을 사용한다
 	public void delete(@Param("mid") String mid, @Param("psid") String psid); 
 	public void update(@Param("mid") String mid, @Param("psid") String psid, @Param("pquantity") long pquantity); 
 
 }
-
-//public int insertProduct(ShoppingBag shoppingBag);
-//public int updateProduct(ShoppingBag shoppingBag);
-//public int deleteProduct(int sbno);
-//public int selectProductNo(ShoppingBag shoppingBag);
-//public List<ShoppingBag> selectShoppingProducts(int mno);
-//public ShoppingBag selectShoppingProduct(int sbno);
