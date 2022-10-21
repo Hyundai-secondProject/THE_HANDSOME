04111c49a069df483728675d2fca860548428184
kosa <kosa@DESKTOP-I92AVJE>
2022-10-21 16:23:05 +0900

    [feat] memberService join edit, passwordcheck add (#66)

--- src/main/java/com/kosa/domain/member/MemberVO.java
+++ src/main/java/com/kosa/domain/member/MemberVO.java
@@ -1,7 +1,12 @@
 package com.kosa.domain.member;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,17 +18,18 @@ import lombok.ToString;
  * @version 1.0
  * 
  * <pre>
- * 수정일              수정자                   수정내용
+ * 수정일                    수정자                   수정내용
  * ----------  --------    ---------------------------
- * 2022.10.13     공통              최초 생성
+ * 2022.10.13      공통             	최초 생성
+ * 2022.10.20	신기원		컬럼 주석 수정
  * </pre>
  */
 
 @Getter
 @Setter
 @ToString
-public class MemberVO {
-	
+public class MemberVO{
+
 	private String mid;	// 회원아이디
 	private String mpassword;  // 회원비밀번호
 	private String mname;  // 회원이름
@@ -38,9 +44,11 @@ public class MemberVO {
 	private String mrefid;  // 추천인
 	private String mlogintype;  // 로그인 유형
 	private int mtosno;  // 사용자정보제공
-	private int menabled;  // ? 활성화 여부 휴면계정...
-	private String mrole;  // 유저 or admin
+	private int menabled;  // 계정 활성화 여부
+	private String mrole;  // member or admin
 	private int mgrade;   // 회원등급
 	private int mmileage;   // 마일리지
+	
+	
 	
 }
--- src/main/java/com/kosa/service/MemberServicelmpl.java
+++ src/main/java/com/kosa/service/MemberServicelmpl.java
@@ -7,6 +7,8 @@ import com.kosa.mapper.MemberMapper;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 /**
  * @author 신기원
@@ -14,9 +16,10 @@ import org.springframework.stereotype.Service;
  * @version 1.0
  * 
  * <pre>
- * 수정일              수정자                   수정내용
+ * 수정일                    수정자                   수정내용
  * ----------  --------    ---------------------------
- * 2022.10.14     신기원              최초 생성
+ * 2022.10.14    신기원                 최초 생성
+ * 2022.10.18    신기원                 패스워드 암호화, 입력 값과 DB 값 일치 확인
  * </pre>
  */
 @Service
@@ -26,18 +29,28 @@ public class MemberServicelmpl implements MemberService {
 
 	private final MemberMapper memberMapper;
 	
+//	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	//패스워드 암호화를 위한 스프링 빈 객체
+	private final PasswordEncoder encoder;
+	
+	//회원 가입 메소드 아이디 값을 반환
 	@Transactional
 	@Override
 	public String join(MemberVO memberVO) {
+		
+		//입력 받은 패스워드 암호화
+		memberVO.setMpassword(encoder.encode(memberVO.getMpassword()));
+		
 		memberMapper.join(memberVO);
 		return memberVO.getMid();
 	}
-
+	
+	//입력 받은 패스워드와 DB의 패스워드 체크 메서드
 	@Override
-	public MemberVO Login(String mid, String mpassword) {
+	public MemberVO passwordCheck(String mid, String mpassword) {
 		MemberVO memberVO =  memberMapper.findById(mid);
-		if(! memberVO.getMpassword().equals(mpassword)) return null;
-		else return memberVO;
+		if(encoder.matches(mpassword, memberVO.getMpassword())) return memberVO;
+		else return null;
 	}
 
 }
